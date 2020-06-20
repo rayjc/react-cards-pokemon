5e44bbc0706f9d2a561c5c2c7cab1c55582a7c14
Ray Chung <ray.j.chung@gmail.com>
2020-06-19 17:30:26 -0700

    Add axios hook; refactor PlayingCardList and PokeDex components.

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import uuid from "uuid";
-import axios from "axios";
+import React from "react";
+import { useAxios } from "./hooks";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
 import "./PokeDex.css";
@@ -9,13 +8,8 @@ import "./PokeDex.css";
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
-  };
+  const [pokemon, addPokemon] = useAxios("https://pokeapi.co/api/v2/pokemon/");
+
   return (
     <div className="PokeDex">
       <div className="PokeDex-buttons">
@@ -23,13 +17,13 @@ function PokeDex() {
         <PokemonSelect add={addPokemon} />
       </div>
       <div className="PokeDex-card-area">
-        {pokemon.map(cardData => (
+        {pokemon.map(card => (
           <PokemonCard
-            key={cardData.id}
-            front={cardData.sprites.front_default}
-            back={cardData.sprites.back_default}
-            name={cardData.name}
-            stats={cardData.stats.map(stat => ({
+            key={card.id}
+            front={card.data.sprites.front_default}
+            back={card.data.sprites.back_default}
+            name={card.data.name}
+            stats={card.data.stats.map(stat => ({
               value: stat.base_stat,
               name: stat.stat.name
             }))}
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import uuid from "uuid";
+import axiosErrorHandler from './util';
 
 
 const useFlip = (initialBool = true) => {
@@ -10,4 +13,21 @@ const useFlip = (initialBool = true) => {
 }
 
 
-export { useFlip };
+const useAxios = (baseUrl) => {
+  const [data, setData] = useState([]);
+
+  // make sure to wrap calls to exclude the event object
+  const addData = async (endpoint = '') => {
+    try {
+      const res = await axios.get(baseUrl + endpoint);
+      setData(d => [...d, { data: res.data, error: null, id: uuid() }]);
+    } catch (error) {
+      axiosErrorHandler(error);
+      setData(d => [...d, { data: null, error, id: uuid() }]);
+    }
+  }
+
+  return [data, addData];
+}
+
+export { useAxios, useFlip };
